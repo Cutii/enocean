b055f9bb00d6f8437832b008fe16395360132022
Pierrick Mirabel <pmirabel@cutii.io>
2019-05-20 09:23:16 +0200

    :recycle: Modified main example to adapt changes from Refacto of encoean communicator

--- examples/main.rs
+++ examples/main.rs
@@ -4,6 +4,8 @@ use std::time::Duration;
 
 extern crate enocean;
 use enocean::enocean::*;
+use enocean::communicator::EnoceanCommunicator;
+
 
 fn main() {
     // Just to show how much enocean serial packets were received
@@ -11,35 +13,32 @@ fn main() {
     // Just to show how much enocean serial packets were sended
     let mut nb_sended = 0;
     // For now, this variable is hardcoded
-    let port_name = "/dev/tty.usbserial-FT1ZKA73".to_string(); //Get this from env?
-                                                               // Communication channels based on MPSC (1 to send, 1 to receive esp3 packets)
-    let (enocean_emiter, enocean_event_receiver) = mpsc::channel();
-    let (enocean_command_receiver, enocean_commander) = mpsc::channel();
+    let port_name = "/dev/tty.usbserial-FT1ZKA73"; // Get this from env?
+                                                   // Communication channels based on MPSC (1 to send, 1 to receive esp3 packets)
 
-    // Create a thread to interact (both ways) with serial port
+    // Create a communicator to interact (both ways) with serial port
     // The interaction is achieved thanks to 2 channels (std::sync lib)
-    let _enocean_listener = thread::spawn(move || {
-        enocean::communicator::start(port_name, enocean_emiter, enocean_commander);
-    });
-
+    let enocean_communicator = EnoceanCommunicator::new(port_name);
+  
     // Create a thread to send commands every second
-    let _command_emiter = thread::spawn(move || loop {
-        match enocean_command_receiver.send(enocean::eep::create_smart_plug_command(
-            [0x05, 0x0a, 0x3d, 0x6a],
-            enocean::eep::D201CommandList::QueryPower,
-        )) {
-            Ok(_t) => {}
-            Err(e) => eprintln!("erreur lors de l'envoi : {:?}", e),
-        }
-        nb_sended = nb_sended + 1;
-        println!("---> SENDED : {}", nb_sended);
-        thread::sleep(Duration::from_millis(10000));
-    });
+    // TODO : Change create_xxx_command return type from vecU8 to ESP3 ==> add fromVec trait to ESP3
+    // let _command_emiter = thread::spawn(move || loop {
+    //     match enocean_communicator.enocean_sender.send(enocean::eep::create_smart_plug_command(
+    //         [0x05, 0x0a, 0x3d, 0x6a],
+    //         enocean::eep::D201CommandList::QueryPower,
+    //     )) {
+    //         Ok(_t) => {}
+    //         Err(e) => eprintln!("erreur lors de l'envoi : {:?}", e),
+    //     }
+    //     nb_sended = nb_sended + 1;
+    //     println!("---> SENDED : {}", nb_sended);
+    //     thread::sleep(Duration::from_millis(10000));
+    // });
 
     // Loop to check if we received something. If everything went well during "send phase",
     // we should receive a Reponse type ESP3 packet with ReturnCode 0 each time we send a command.
     loop {
-        let message = enocean_event_receiver.try_recv();
+        let message = enocean_communicator.enocean_receiver.lock().unwrap().try_recv();
         match message {
             Ok(esp3_packet) => {
                 print_esp3(esp3_packet);
