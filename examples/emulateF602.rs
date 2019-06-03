87828e80532a6fe5d13e971df22cbfde8fd3f64d
Pierrick Mirabel <pmirabel@cutii.io>
2019-06-03 10:36:07 +0200

    :alembic: implemented F602 telegram emulation

--- examples/emulateF602.rs
+++ examples/emulateF602.rs
@@ -12,14 +12,34 @@ fn main() {
     let port_name = "/dev/tty.usbserial-FTWTOH0A".to_string(); //Get this from env?
                                                                // Communication channels based on MPSC (1 to send, 1 to receive esp3 packets)
     let (enocean_emiter, enocean_event_receiver) = mpsc::channel();
-    let (_enocean_command_receiver, enocean_commander) = mpsc::channel();
+    let (enocean_command_receiver, enocean_commander) = mpsc::channel();
 
     // Create a thread to interact (both ways) with serial port
     // The interaction is achieved thanks to 2 channels (std::sync lib)
     let _enocean_listener = thread::spawn(move || {
         enocean::communicator::start(port_name, enocean_emiter, enocean_commander);
     });
+   let F602_emulate_close = enocean::eep::create_f60201_telegram(
+            enocean::eep::F602EmulateCommand::MoveBlindClosed, // QueryEnergy, Off, On
+        ).unwrap();
+    let F602_emulate_open = enocean::eep::create_f60201_telegram(
+            enocean::eep::F602EmulateCommand::MoveBlindOpen, // QueryEnergy, Off, On
+        ).unwrap();
 
+    let _command_emiter = thread::spawn(move || loop {
+        match enocean_command_receiver.send(F602_emulate_close.clone()) {
+            Ok(_t) => {}
+            Err(e) => eprintln!("erreur lors de l'envoi : {:?}", e),
+        }
+        
+        thread::sleep(Duration::from_millis(2000));
+        match enocean_command_receiver.send(F602_emulate_open.clone()) {
+            Ok(_t) => {}
+            Err(e) => eprintln!("erreur lors de l'envoi : {:?}", e),
+        }
+        
+        thread::sleep(Duration::from_millis(2000));
+        });
     // Loop to check if we received something. If everything went well during "send phase",
     // we should receive a Reponse type ESP3 packet with ReturnCode 0 each time we send a command.
     loop {
