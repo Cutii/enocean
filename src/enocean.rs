338d401f7d986cb76b92e0073fd0c6b24cdff046
Maxime Augier <max@xolus.net>
2022-11-21 17:59:46 +0100

    Fix markdown formatting in doc

--- src/enocean.rs
+++ src/enocean.rs
@@ -3,7 +3,7 @@
 //! Enocean : ([official website](https://www.enocean.com/en/)) is a Radio protocol for Smart Home / Buildings devices.
 //!
 //! This lib is a rust implementation of Enocean Serial Protocol, which you can find here: [ESP3](https://www.enocean.com/esp)
-//! You can use this library with any compatible EnOcean Radio Gateway (eg. [USB300 gateway]https://www.enocean.com/en/enocean-modules/details/usb-300-oem/)).
+//! You can use this library with any compatible EnOcean Radio Gateway (eg. [USB300 gateway](https://www.enocean.com/en/enocean-modules/details/usb-300-oem/)).
 //!
 //!
 //! :warning: **This lib is still under construction** :warning:
@@ -18,15 +18,15 @@
 //! For now this lib allow you to create an ESP struct from an incomming bytes vector.
 //!
 //! **Supported packet type** :
-//!  [x]   Radio ERP1 : 0x01  
-//!  [x]   Response : 0x02  
-//!  [ ]   radio_sub_tel : 0x03  
-//!  [ ]   event : 0x04  
-//!  [ ]   common_command : 0x05  
-//!  [ ]   smart_ack_command : 0x06  
-//!  [ ]   remote_man_command : 0x07  
-//!  [ ]   radio_message : 0x09  
-//!  [ ]   radio_advanced : 0x0a  
+//!  - [x]   Radio ERP1 : 0x01  
+//!  - [x]   Response : 0x02  
+//!  - [ ]   radio_sub_tel : 0x03  
+//!  - [ ]   event : 0x04  
+//!  - [ ]   common_command : 0x05  
+//!  - [ ]   smart_ack_command : 0x06  
+//!  - [ ]   remote_man_command : 0x07  
+//!  - [ ]   radio_message : 0x09  
+//!  - [ ]   radio_advanced : 0x0a  
 //!
 //! ## License
 //! [license]: #license
