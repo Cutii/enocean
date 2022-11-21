8a62b45ea277e65c081bdfbf0a409f4c469e1707
Maxime Augier <max@xolus.net>
2022-11-21 19:43:56 +0100

    Improve formatting and documentation

--- src/frame.rs
+++ src/frame.rs
@@ -1,11 +1,47 @@
 
 //! # EnOcean ESP3 frames
-//! 
-//! A low-level interface to ESP3 frames. 
-//! 
+//!
+//! A low-level interface to ESP3 frames.
+//!
 //! This module will handle frame synchronization, memory allocation, and CRC checks.
-//! 
+//!
 //! Data, optional data, and packet type are exposed as raw values and byte slices.
+//!
+//! Read a frame from a serial port:
+//! ```no_run
+//! # use enocean::frame::*;
+//! use std::io::BufReader;
+//! let serial_port = serialport::new("/dev/ttyUSB0", 57600).open()?;
+//! let mut serial_port = BufReader::new(serial_port); // Buffer the reader
+//! loop {
+//!     let frame = ESP3Frame::read_from(&mut serial_port)?;
+//! }
+//! # Ok::<(),Box<dyn std::error::Error>>(())
+//! ```
+//!
+//! To parse a frame already in memory, use a `&mut &[u8]`:
+//! ```
+//! # use enocean::frame::*;
+//! let frame_bin = vec![85, 0, 10, 7, 1, 235, 165, 16, 8, 70, 128, 5, 17, 114, 247, 0, 1, 255, 255, 255, 255, 55, 0, 55];
+//! let frame = ESP3Frame::read_from(&mut &frame_bin[..]).unwrap();
+//! assert_eq!(frame.packet_type(), 0x01);
+//! assert_eq!(frame.data(), &[165, 16, 8, 70, 128, 5, 17, 114, 247, 0]);
+//! assert_eq!(frame.optional_data(), &[1, 255, 255, 255, 255, 55, 0]);
+//! ```
+//!
+//! Build a frame from existing pieces and send it, without copying:
+//!
+//! ```
+//! # use enocean::frame::*;
+//! let data = &[165, 16, 8, 70, 128, 5, 17, 114, 247, 0];
+//! let optional_data = &[1, 255, 255, 255, 255, 55, 0];
+//!
+//! let frame = ESP3FrameRef { packet_type: 1, data, optional_data };
+//! # let mut serial_port = vec![];
+//! frame.write_to(&mut serial_port).unwrap();
+//! # assert_eq!(&serial_port[..], &[85, 0, 10, 7, 1, 235, 165, 16, 8, 70, 128, 5, 17, 114, 247, 0, 1, 255, 255, 255, 255, 55, 0, 55]);
+//! ```
+//!
 
 use std::borrow::Borrow;
 
@@ -36,15 +72,15 @@ pub struct ESP3FrameRef<'a> {
 impl ESP3Frame {
 
     /// Build an owned frame from its component.
-    /// 
+    ///
     /// This copies the data. Prefer creating an `ESP3FrameRef` directly, and calling `write()` on it.
     pub fn assemble(packet_type: u8, data: &[u8], optional_data: &[u8]) -> Self {
         ESP3FrameRef { packet_type, data, optional_data }.to_owned()
     }
 
-    /// Read a frame from a buffered reader. Will perform header synchronization. Allocates exactly the space needed. 
+    /// Read a frame from a buffered reader. Will perform header synchronization. Allocates exactly the space needed.
     pub fn read_from(reader: &mut impl std::io::BufRead) -> Result<Self, FrameReadError> {
-      
+
         let header = loop {  // Synchronize with start of packet
             let buf = reader.fill_buf()?;
             if buf.len() == 0 { return Err(FrameReadError::EOF) }
@@ -59,7 +95,7 @@ impl ESP3Frame {
                 reader.consume(1);
                 continue;
             }
-            
+
             break buf;
         };
 
@@ -68,7 +104,7 @@ impl ESP3Frame {
         let data_length = ((header[1] as usize) << 8) + (header[2] as usize);
         let optional_data_length = header[3] as usize;
         let packet_type = header[4];
-        
+
         // Allocate an appropriate buffer
         let total_length = 6 + data_length + optional_data_length + 1;
         let mut frame = vec![0; total_length];
@@ -95,9 +131,9 @@ impl ESP3Frame {
 
     /// The optional data
     pub fn optional_data(&self) -> &[u8] {
-        &self.frame[6+self.data_length..][..self.optional_data_length]    
+        &self.frame[6+self.data_length..][..self.optional_data_length]
     }
-    
+
     /// Borrows an ESP3Frame as an ESPFrameRef
     pub fn as_ref(&self) -> ESP3FrameRef {
         ESP3FrameRef { packet_type: self.packet_type
@@ -154,8 +190,8 @@ impl<'a> ESP3FrameRef<'a> {
         let mut frame = Vec::with_capacity(6 + self.data.len() + self.optional_data.len() + 1);
         self.write_to(&mut frame).unwrap();   // Writing to a preallocated Vec should never fail
 
-        ESP3Frame { packet_type: self.packet_type, 
-                    data_length: self.data.len(), 
+        ESP3Frame { packet_type: self.packet_type,
+                    data_length: self.data.len(),
                     optional_data_length: self.optional_data.len(),
                     frame }
 
